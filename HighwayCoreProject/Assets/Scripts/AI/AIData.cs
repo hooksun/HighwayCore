b6425dd78b2530cb3842960c5c7e8d63fb6a475b
Gilang Kurniawan <90315796+Gilangk1902@users.noreply.github.com>
2022-11-02 01:03:15 +0700

    Test AI
    basic chasing, attacking.
    
    To Do Next = Slow Projectile

---
+++ HighwayCoreProject/Assets/Scripts/AI/AIData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="AI", menuName ="NPC/AI")]
+public class AIData : ScriptableObject
+{
+    public string name;
+    public float fireRate; // seconds/shot
+    public float movementSpeed;
+    public int shootingBurst; //Berapa banyak tembakan tiap kali serang
+    public bool readyToFire;
+
+    public float chaseRange;
+    public float attackRange;
+}
