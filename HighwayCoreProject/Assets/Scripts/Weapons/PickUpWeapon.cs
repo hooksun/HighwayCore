80bbe158a1db62ff0e6f832b0df485d6c111cb84
Gilang Kurniawan <90315796+Gilangk1902@users.noreply.github.com>
2022-11-04 16:52:00 +0700

    Add Sniper

--- HighwayCoreProject/Assets/Scripts/Weapons/PickUpWeapon.cs
+++ HighwayCoreProject/Assets/Scripts/Weapons/PickUpWeapon.cs
@@ -15,7 +15,8 @@ public class PickUpWeapon : MonoBehaviour
         if(Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hit)){
             AddWeaponToPlayer weapon = hit.collider.gameObject.GetComponent<AddWeaponToPlayer>();
             if((hit.collider.name == "AssaultRifleObj" ||
-            hit.collider.name == "ShotgunObj")&& weapon.inRange){
+            hit.collider.name == "ShotgunObj" ||
+            hit.collider.name == "SniperObj")&& weapon.inRange){
                 Debug.Log("E to interact");
                 if(Input.GetKey(KeyCode.E)){
                     weapon.pickedUp = true;
