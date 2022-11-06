f8d7b8eaebd0a57b8b8c443494700a1372c585a4
Gilang Kurniawan <90315796+Gilangk1902@users.noreply.github.com>
2022-11-06 19:24:36 +0700

    weapon pickup range fix

--- HighwayCoreProject/Assets/Scripts/Weapons/AddWeaponToPlayer.cs
+++ HighwayCoreProject/Assets/Scripts/Weapons/AddWeaponToPlayer.cs
@@ -6,18 +6,20 @@ public class AddWeaponToPlayer : MonoBehaviour
 {
     public bool inRange;
     public bool pickedUp;
+    public float radius;
     public LayerMask playerMask;
     public GunData thisWeapon;
     // Start is called before the first frame update
     void Start()
     {
         pickedUp = false;
+        inRange = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        inRange = Physics.CheckSphere(transform.position, 2f, playerMask);
+        inRange = Physics.CheckSphere(transform.position, radius, playerMask);
 
         if(pickedUp && !thisWeapon.available){
             SendToPlayer();
