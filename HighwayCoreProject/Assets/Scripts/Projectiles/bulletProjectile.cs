997f9a3e064dffa3c620c6c2b7f79e55a4eda89a
Gilang Kurniawan <90315796+Gilangk1902@users.noreply.github.com>
2022-11-08 22:27:18 +0700

    Bullet Pool

--- HighwayCoreProject/Assets/Scripts/Projectiles/bulletProjectile.cs
+++ HighwayCoreProject/Assets/Scripts/Projectiles/bulletProjectile.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bulletProjectile : MonoBehaviour
+public class bulletProjectile : MonoBehaviour, IPooledBullet
 {
     Vector3 startPos;
     Vector3 currPos;
     [SerializeField] LayerMask groundMask;
     GunScript gundScript;
-    private bool collide;
+    private bool hitSomething;
 
-    void Start()
+    public void onObjectSpawn()
     {   
         startPos = transform.position;
         gundScript = GameObject.Find("Weapon Holder").GetComponent<GunScript>();
@@ -19,22 +19,12 @@ public class bulletProjectile : MonoBehaviour
     void Update(){
         currPos = transform.position;
         travel();
-        
-        DestroyObj();
     }
 
     void travel(){
         transform.position += transform.forward * Time.deltaTime * gundScript.gunData.bulletSpeed; 
     }
-    void DestroyObj(){
-        collide = Physics.CheckSphere(transform.position, .1f, groundMask);
-        if(collide){
-            Destroy(gameObject);
-        }
-        if(Vector3.Distance(startPos, currPos) > 10f){
-            Destroy(gameObject);
-        }
-    }
+    
 
     
 }
