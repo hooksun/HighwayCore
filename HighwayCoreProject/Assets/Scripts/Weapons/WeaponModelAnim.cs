fa6ecfb756590ad305ebf3b941a9c3971c50fe00
Gilang Kurniawan <90315796+Gilangk1902@users.noreply.github.com>
2023-01-03 22:40:44 +0700

    magazine anim test
    AR only

---
+++ HighwayCoreProject/Assets/Scripts/Weapons/WeaponModelAnim.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponModelAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator anim;
+    public GunScript gun;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gun.isReloading){
+            anim.SetBool("reload", true);
+        }
+        else{
+            anim.SetBool("reload", false);
+        }
+    }
+}
